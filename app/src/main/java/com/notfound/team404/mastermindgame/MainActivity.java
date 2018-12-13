0be917031c3848f8f0ea061c033f1e50d333636f
Luong Cong Tam <congtam.luong0406@gmail.com>
2018-12-13 23:42:29 +0700

    Remove Fullscreen Activity, Add Basic Activity

---
+++ app/src/main/java/com/notfound/team404/mastermindgame/MainActivity.java
@@ -0,0 +1,22 @@
+package com.notfound.team404.mastermindgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_main);
+
+    }
+}
