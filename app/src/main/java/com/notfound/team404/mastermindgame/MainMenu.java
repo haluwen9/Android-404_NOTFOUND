3b691f314b22f876ddcf45fd980a87030cb99d21
Nguyen Vuong <vlmnguyen98@gmail.com>
2018-12-14 14:34:07 +0700

    Added Gameplay Activity
    - Added Gameplay Activity
    - Added onClick for btnPlay in MainMenu

--- app/src/main/java/com/notfound/team404/mastermindgame/MainMenu.java
+++ app/src/main/java/com/notfound/team404/mastermindgame/MainMenu.java
@@ -1,11 +1,13 @@
 package com.notfound.team404.mastermindgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -15,5 +17,14 @@ public class MainMenu extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void onClick(View view) {
+        TextView textView = (TextView)view;
+        String id=getResources().getResourceEntryName(textView.getId());
+        if (id.equals("btnPlay")) {
+            Intent intent = new Intent(this, Gameplay.class);
+            startActivity(intent);
+        }
     }
 }
