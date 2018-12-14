e6f4e4cd56cc0910035714efb54392599cf07026
HelpMe-Pls <leekhoi5319990@gmail.com>
2018-12-14 18:14:11 +0700

    Add Credits

---
+++ app/src/main/java/com/notfound/team404/mastermindgame/Credits.java
@@ -0,0 +1,36 @@
+package com.notfound.team404.mastermindgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Credits extends AppCompatActivity {
+
+    TextView tv_credits =  findViewById(R.id.tv_credits);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+
+
+        String text = "";
+        try {
+            InputStream inp = getAssets().open("credits.txt");
+            int size = inp.available();
+            byte[] buffer = new byte[size];
+            inp.read(buffer);
+            inp.close();
+            text = new String(buffer);
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        tv_credits.setText(text);
+
+    }
+
+}
